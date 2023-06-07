f9ac180e0c504c56f3bd637d577dab3cd436aa15
Bruno Paveglio <brunopaveglio@air-de-bruno.speedport.ip>
2023-06-07 21:08:43 +0200

    Adding profile pic

--- src/components/about-me/AboutMe.component.js
+++ src/components/about-me/AboutMe.component.js
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import {useTranslation} from 'react-i18next';
 import Button from '../library/Button/Button.component';
 import styles from './AboutMe.module.scss';
@@ -8,19 +9,24 @@ const AboutMe = () => {
 	return (
 		<div className={styles['container']}>
 			<div className={styles['inner-container']}>
-				<h2 className={styles['title']}>{t('about.title')}</h2>
-				<div className={styles['description']}>
-					<p>{t('about.first')}</p>
-					<p>{t('about.second')}</p>
+				<div className={styles['image-container']}>
+					<img src='/profile.png' />
 				</div>
-				<a
-					href='/Paveglio-SoftwareDeveloper-2023.pdf'
-					target='_blank'
-					rel='noreferrer noopener'
-					aria-label='Resume/CV'
-				>
-					<Button>{t('navbar.resume')}</Button>
-				</a>
+				<div className={styles['data-container']}>
+					<h2 className={styles['title']}>{t('about.title')}</h2>
+					<div className={styles['description']}>
+						<p>{t('about.first')}</p>
+						<p>{t('about.second')}</p>
+					</div>
+					<a
+						href='/Paveglio-SoftwareDeveloper-2023.pdf'
+						target='_blank'
+						rel='noreferrer noopener'
+						aria-label='Resume/CV'
+					>
+						<Button>{t('navbar.resume')}</Button>
+					</a>
+				</div>
 			</div>
 		</div>
 	);
