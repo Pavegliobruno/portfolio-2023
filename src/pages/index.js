499acc0acde7d3eb80af214bc599cc0d316d0052
Bruno Paveglio <brunopaveglio@air-de-bruno.speedport.ip>
2023-06-07 15:34:39 +0200

    Updating index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,11 @@ import React, {useState} from 'react';
 import {Element} from 'react-scroll';
 import Navbar from 'src/components/navbar/Navbar.component';
 import Hero from 'src/components/hero/Hero.component';
-import AboutMe from 'src/components/about-me/AboutMe.component';
+/* import AboutMe from 'src/components/about-me/AboutMe.component';
 import Projects from 'src/components/projects/Projects.component';
 import Testimonials from 'src/components/testimonials/Testimonials.component';
 import Contact from 'src/components/contact/Contact.component';
-import Footer from 'src/components/footer/Footer.component';
+import Footer from 'src/components/footer/Footer.component'; */
 
 const index = () => {
 	const [starsBackgroundEnabled, setStarsBackgroundEnabled] = useState(true);
@@ -15,7 +15,7 @@ const index = () => {
 		<div>
 			<Navbar setStarsBackgroundEnabled={setStarsBackgroundEnabled} />
 			<Hero starsBackgroundEnabled={starsBackgroundEnabled} />
-			<Element name='about'>
+			{/* <Element name='about'>
 				<AboutMe />
 			</Element>
 			<Element name='projects'>
@@ -27,7 +27,7 @@ const index = () => {
 			<Element name='contact'>
 				<Contact />
 			</Element>
-			<Footer />
+			<Footer /> */}
 		</div>
 	);
 };
