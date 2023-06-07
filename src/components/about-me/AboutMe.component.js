c27911c7070d1349c0e7dea84789a44fbbcfed5f
Bruno Paveglio <brunopaveglio@air-de-bruno.speedport.ip>
2023-06-07 15:49:49 +0200

    Adding AboutMe component

---
+++ src/components/about-me/AboutMe.component.js
@@ -0,0 +1,29 @@
+import {useTranslation} from 'react-i18next';
+import Button from '../library/Button/Button.component';
+import styles from './AboutMe.module.scss';
+
+const AboutMe = () => {
+	const [t] = useTranslation('global');
+
+	return (
+		<div className={styles['container']}>
+			<div className={styles['inner-container']}>
+				<h2 className={styles['title']}>{t('about.title')}</h2>
+				<div className={styles['description']}>
+					<p>{t('about.first')}</p>
+					<p>{t('about.second')}</p>
+				</div>
+				<a
+					href='/Paveglio-SoftwareDeveloper-2023.pdf'
+					target='_blank'
+					rel='noreferrer noopener'
+					aria-label='Resume/CV'
+				>
+					<Button>{t('navbar.resume')}</Button>
+				</a>
+			</div>
+		</div>
+	);
+};
+
+export default AboutMe;
