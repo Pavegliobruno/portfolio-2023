517be427ea15146abf14ceec7d452baf0a6b790f
Bruno Paveglio <brunopaveglio@air-de-bruno.speedport.ip>
2023-06-07 15:31:06 +0200

    Adding all copy and texts

---
+++ src/data/projects.js
@@ -0,0 +1,71 @@
+export const projects = [
+	{
+		name: 'NFTs Drop - Whitelabel',
+		img: [
+			'https://res.cloudinary.com/dlexbrcrv/image/upload/v1662455118/Proyects/Portfolio/drops_1_h9yzxv.png',
+			'https://res.cloudinary.com/dlexbrcrv/image/upload/v1662455118/Proyects/Portfolio/drops_2_hzkgsg.png',
+			'https://res.cloudinary.com/dlexbrcrv/image/upload/v1662455119/Proyects/Portfolio/drops_3_hpgqbm.png',
+			'https://res.cloudinary.com/dlexbrcrv/image/upload/v1662455119/Proyects/Portfolio/drops_4_cvdeit.png',
+		],
+		description: 'dropWhitelabel',
+		github: '',
+		web: 'https://drops-stg-client.waytoodigital.com/',
+	},
+	{
+		name: 'NFTs Drop - Local weather',
+		img: [
+			'https://res.cloudinary.com/dlexbrcrv/image/upload/v1662454363/Proyects/Portfolio/l1_jwmovq.png',
+			'https://res.cloudinary.com/dlexbrcrv/image/upload/v1662454362/Proyects/Portfolio/l3_fhmccf.png',
+			'https://res.cloudinary.com/dlexbrcrv/image/upload/v1662454362/Proyects/Portfolio/l2_zc1zzr.png',
+		],
+		description: 'dropLocalWeather',
+		github: '',
+		web: 'https://nft.drinklocalweather.com/',
+	},
+	{
+		name: 'NFTs Drop - Breadaverse',
+		img: [
+			'https://res.cloudinary.com/dlexbrcrv/image/upload/v1662454702/Proyects/Portfolio/b1_i7yo23.png',
+			'https://res.cloudinary.com/dlexbrcrv/image/upload/v1662454702/Proyects/Portfolio/b2_bdbpf2.png',
+			'https://res.cloudinary.com/dlexbrcrv/image/upload/v1662454702/Proyects/Portfolio/b3_ithf4w.png',
+		],
+		description: 'dropBreadaverse',
+		github: '',
+		web: 'https://breadaverse.xyz/',
+	},
+
+	{
+		name: 'Store! E-commerce.',
+		img: [
+			'https://res.cloudinary.com/dlexbrcrv/image/upload/v1623695694/Proyects/Portfolio/S-1_hdyy15.png',
+			'https://res.cloudinary.com/dlexbrcrv/image/upload/v1623705426/Proyects/Portfolio/S-2_emotxo.png',
+			'https://res.cloudinary.com/dlexbrcrv/image/upload/v1623705426/Proyects/Portfolio/S-3_z6x65n.png',
+			'https://res.cloudinary.com/dlexbrcrv/image/upload/v1623705426/Proyects/Portfolio/S-4_rlvx5f.png',
+		],
+		description: 'store',
+		github: 'https://github.com/josegarrera/ecommerce',
+		web: '',
+	},
+	{
+		name: 'Videogames!',
+		img: [
+			'https://res.cloudinary.com/dlexbrcrv/image/upload/v1623705095/Proyects/Portfolio/V-1_hbob53.png',
+			'https://res.cloudinary.com/dlexbrcrv/image/upload/v1623705093/Proyects/Portfolio/V-2_wq6klv.png',
+			'https://res.cloudinary.com/dlexbrcrv/image/upload/v1623705091/Proyects/Portfolio/V-3_ewrzpe.png',
+			'https://res.cloudinary.com/dlexbrcrv/image/upload/v1623705089/Proyects/Portfolio/V-4_zwlkel.png',
+		],
+		description: 'videogames',
+		github: 'https://github.com/Pavegliobruno/Videogames',
+		web: '',
+	},
+	{
+		name: 'My portfolio.',
+		img: [
+			'https://res.cloudinary.com/dlexbrcrv/image/upload/v1624052305/Proyects/Portfolio/Screenshot_1_njfbuy.png',
+			'https://res.cloudinary.com/dlexbrcrv/image/upload/v1623695932/Proyects/Portfolio/P-1_sudmtl.png',
+		],
+		description: 'portfolio',
+		github: 'https://github.com/Pavegliobruno/portfolio_paveglio',
+		web: '',
+	},
+];
---
+++ src/data/testimonials.js
@@ -0,0 +1,38 @@
+export const testimonials = [
+	{
+		img: 'https://res.cloudinary.com/dlexbrcrv/image/upload/v1623352008/Proyects/Portfolio/1619619418159_s0aupn.jpg',
+		name: 'Facundo Cordoba',
+		title: 'Fullstack Developer ',
+		testimonial: 'facundo',
+	},
+	{
+		img: 'https://res.cloudinary.com/dlexbrcrv/image/upload/v1623352007/Proyects/Portfolio/1621425093159_w7bh5t.jpg',
+		name: 'Camila Alvarez',
+		title: 'Backend developer ',
+		testimonial: 'camila',
+	},
+	{
+		img: 'https://res.cloudinary.com/dlexbrcrv/image/upload/v1623352009/Proyects/Portfolio/1620350023982_vnsx6u.jpg',
+		name: 'Alonso Diaz',
+		title: 'React developer ',
+		testimonial: 'alonso',
+	},
+	{
+		img: 'https://res.cloudinary.com/dlexbrcrv/image/upload/v1623756978/Proyects/Portfolio/1614117745178_xicwm7.jpg',
+		name: 'Juan Martin Cereceda',
+		title: 'Full Stack Developer ',
+		testimonial: 'juanma',
+	},
+	{
+		img: 'https://res.cloudinary.com/dlexbrcrv/image/upload/v1623781143/Proyects/Portfolio/1621346044376_h7gsov.jpg',
+		name: 'Pedro Contreras',
+		title: 'Fullstack developer ',
+		testimonial: 'pedro',
+	},
+	{
+		img: 'https://res.cloudinary.com/dlexbrcrv/image/upload/v1626133012/Proyects/Portfolio/TPRS7H4PN-U018F3CNRJ6-853898ff17ab-512_a4w78k.png',
+		name: 'Martina Scomazzon',
+		title: 'Computer engineer',
+		testimonial: 'martu',
+	},
+];
