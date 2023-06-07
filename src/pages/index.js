d71cdbd4aeffebeb648f22e64571de81a75c2a8c
Bruno Paveglio <brunopaveglio@air-de-bruno.speedport.ip>
2023-06-07 15:41:37 +0200

    Adding Homepage component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,35 +1,7 @@
-import React, {useState} from 'react';
-import {Element} from 'react-scroll';
-import Navbar from 'src/components/navbar/Navbar.component';
-import Hero from 'src/components/hero/Hero.component';
-/* import AboutMe from 'src/components/about-me/AboutMe.component';
-import Projects from 'src/components/projects/Projects.component';
-import Testimonials from 'src/components/testimonials/Testimonials.component';
-import Contact from 'src/components/contact/Contact.component';
-import Footer from 'src/components/footer/Footer.component'; */
+import Homepage from './Homepage';
 
 const index = () => {
-	const [starsBackgroundEnabled, setStarsBackgroundEnabled] = useState(true);
-
-	return (
-		<div>
-			<Navbar setStarsBackgroundEnabled={setStarsBackgroundEnabled} />
-			<Hero starsBackgroundEnabled={starsBackgroundEnabled} />
-			{/* <Element name='about'>
-				<AboutMe />
-			</Element>
-			<Element name='projects'>
-				<Projects />
-			</Element>
-			<Element name='testimonials'>
-				<Testimonials />
-			</Element>
-			<Element name='contact'>
-				<Contact />
-			</Element>
-			<Footer /> */}
-		</div>
-	);
+	return <Homepage />;
 };
 
 export default index;
